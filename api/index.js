19f1254ab04b109c50f51561e0ea08611a307850
Apridinansyah <zedhow54@gmail.com>
2024-06-22 16:04:40 +0800

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+const mongoURI = 'mongodb://localhost:27017/mern-blog';
 
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB is connected...');
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB...', err);
+  });
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
 });
